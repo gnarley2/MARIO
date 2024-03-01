5a2cbc98c9c41f091e8750eaa17eadf9e9bb5ad4
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-01 15:55:37 +1100

    Implemented player direction
    Player turns to face direction its moving in

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,11 @@ public class PlayerMovement : MonoBehaviour
     // [SerializeField] private Rigidbody2D rb;
     private Rigidbody2D rb;
 
+    private Vector2 velocity;
+    private float moveInput;
+    private float jumpForce = 9f;
+    private float moveSpeed = 5f;
+
     // Start is called before the first frame update
     private void Awake()
     {
@@ -21,15 +26,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        float xAxis = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(xAxis * 5f, rb.velocity.y);
+        // float xAxis = Input.GetAxis("Horizontal");
+        // rb.velocity = new Vector2(xAxis * moveSpeed, rb.velocity.y);
+        moveInput = Input.GetAxis("Horizontal");
+        if (moveInput > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
     }
-    
+
     private void Jump()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 9f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rb.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        rb.MovePosition(position);
     }
 }
