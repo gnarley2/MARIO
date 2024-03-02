db02d54c1a20232f30303e104d1087626ca09194
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-02 21:43:46 +1100

    Adding Enemies

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float EnemySpeed = 1f;
+    public new Rigidbody2D rb;
+
+    private Vector2 velocity;
+    public Vector2 direction = Vector2.left;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        enabled = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+
+        //  velocity.x = direction.x * EnemySpeed;
+        //  velocity.y = Physics2D.gravity.y;
+        //// rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        rb.velocity = transform.right * EnemySpeed;
+
+    }
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+    private void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        rb.WakeUp();
+    }
+
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+        rb.Sleep();
+    }
+
+
+
+}
---
+++ Assets/Scripts/EnemyTurnAround.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnAround : MonoBehaviour
+{
+
+
+    void OnTriggerEnter2D(Collider2D enemy)
+    {
+        if (enemy.tag == "Enemy")
+        {
+            enemy.transform.Rotate(0f, 180f, 0f);
+        }
+    }
+}
