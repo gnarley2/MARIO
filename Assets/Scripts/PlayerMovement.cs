52992d6c28dd56cd67ddf823ef66e63e7fe53730
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-02 16:01:54 +1100

    Completed follow camera
    Completed follow camera, player bounds, updated movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,125 +2,79 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
 
-    // [SerializeField] private Rigidbody2D rb;
-    private Rigidbody2D rb;
     private Vector2 velocity;
-    private float moveInput;
-    private float moveSpeed = 5f;
-    private float jumpInput;
-    private float jumpForce = 10f;
-    private bool isGrounded;
-    // public Transform feetPos;
-    // public float circleRadius;
-    // public LayerMask ground;
+    private float inputAxis;
 
-    // public float jumpTime;
-    // private float jumpTimeCounter;
-    // private bool isJumping;
+    public float moveSpeed = 8f;
+    public float jumpHeight = 5f;
+    public float jumpTime = 1f;
 
-    // Start is called before the first frame update
+    public float jumpForce => ((2f * jumpHeight) / (jumpTime / 2f));
+    public float gravity => ((-2f * jumpHeight) / Mathf.Pow((jumpTime / 2f), 2));
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
+
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        // circleRadius = 0.5f;
-
-        // jumpTime = 1;
-        // jumpTimeCounter = 0;
-
-        // isGrounded = true;
-        // isJumping = false;
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Move();
-        Jump(isGrounded);
+        Movement();
 
-        // isGrounded = Physics2D.OverlapCircle(feetPos.position,
-        //     circleRadius);
-        // Jump(isGrounded);
+        grounded = rigidbody.Raycast(Vector2.down);
 
-        // Debug.Log(ground);
-        // Debug.Log(isGrounded);
-    }
-    private void Turn()
-    {
-        if (moveInput > 0)
+        if (grounded)
         {
-            transform.eulerAngles = Vector3.zero;
+            Jump();
         }
-        else if (moveInput < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-    }
-    private void Move()
-    {
-        moveInput = Input.GetAxis("Horizontal");
-        moveInput = Mathf.Min(moveInput, moveSpeed);
-        // rb.velocity = new Vector2(xAxis * moveSpeed, rb.velocity.y);
-        // moveInput = Input.GetAxis("Horizontal");
-        Turn();
-        // velocity = new Vector2(moveInput * moveSpeed, velocity.y);
+
+        ApplyGravity();
     }
 
-    private void Jump(bool isGrounded)
+    private void Movement()
     {
-        if (isGrounded)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                jumpInput++;
-                jumpInput = Mathf.Min(jumpInput, 1);
-            }
-        }
-        else
-        {
-            jumpInput--;
-            jumpInput = Mathf.Max(jumpInput, 0);
-        }
-        // if (isGrounded == true && Input.GetButtonDown("Jump"))
-        // {
-        //     isJumping = true;
-        //     jumpTimeCounter = jumpTime;
-        //     velocity = new Vector2(velocity.x, jumpForce);
-        // }
-        // if (isJumping == true && Input.GetButtonDown("Jump"))
-        // {
-        //     // isGrounded = false;
-        //     if (jumpTimeCounter > 0)
-        //     {
-        //         velocity = new Vector2(velocity.x, jumpForce);
-        //         jumpTimeCounter -= Time.deltaTime;
-        //     }
-        //     else if (jumpTimeCounter < 0)
-        //     {
-        //         isJumping = false;
-        //     }
-        // }
-        // if (Input.GetButtonUp("Jump"))
-        // {
-        //     isJumping = false;
-        // }
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed,
+            moveSpeed * Time.deltaTime);
     }
 
-    void OnCollisionStay2D(Collision2D collision)
+    private void Jump()
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        if (Input.GetButtonDown("Jump"))
         {
-            isGrounded = true;
+            velocity.y = jumpForce;
+            jumping = true;
         }
-        else
-        {
-            isGrounded = false;
-        }
+    }
+
+    private void ApplyGravity()
+    {
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(new Vector2(rb.position.x + moveInput * moveSpeed * Time.fixedDeltaTime,
-            rb.position.y + jumpInput * jumpForce * Time.fixedDeltaTime));
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
+        rigidbody.MovePosition(position);
     }
 }
