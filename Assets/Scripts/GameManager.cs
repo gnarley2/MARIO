c59b278291843add326b97b57b22a1e888015789
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-02 19:36:37 +1100

    Added power up sprites
    Added coin, star, 1 up, & magic mushroom sprites

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int lives { get; private set; }
+    public int coins { get; private set; }  
+
+    void Start()
+    {
+        // NewGame();
+    }
+
+    private void NewGame()
+    {
+        // TODO
+    }
+
+    public void AddCoin()
+    {
+        coins++;
+
+        if (coins == 100)
+        {
+            AddLife();
+            coins = 0;
+        }
+    }
+
+    public void AddLife()
+    {
+        lives++;
+    }
+}
