acda48d2c935a203cf79a0f0f2b2e5de72513025
gnarley2 <112232940+gnarley2@users.noreply.github.com>
2024-03-03 12:42:38 +1100

    Updating the Enemy Movement

--- Assets/Scripts/EnemyMovement2.cs
+++ Assets/Scripts/EnemyMovement2.cs
@@ -1,50 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMovement2 : MonoBehaviour
 {
 
-    public float mMovementSpeed = 3.0f;
-    public bool bIsGoingRight = true;
-
-    private SpriteRenderer _mSpriteRenderer;
-
-    // Start is called before the first frame update
-    void Start()
+    public float MovementSpeed = 3.0f;
+    public bool btravellingRight = true;
+    private Rigidbody2D rb;
+    private SpriteRenderer SpriteRenderer;
+    float FallSpeed = 10.0f;
+    
+    void Awake()
     {
-        _mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        _mSpriteRenderer.flipX = bIsGoingRight;
+        SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        SpriteRenderer.flipX = btravellingRight;
+        enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right;
-        directionTranslation *= Time.deltaTime * mMovementSpeed;
+       Vector3 direction = (btravellingRight) ? transform.right : -transform.right;
+       direction *= Time.deltaTime * MovementSpeed;
 
-        transform.Translate(directionTranslation);
-
-        CheckForWalls();
+      if (enabled)
+       {
+            //Horizontal movement - is continuous
+           transform.Translate(direction);
+            //Turn around when hit pipe or ground
+           TransformFlip();
+            // fall when walk off edge
+           GroundCheck();
+       }
     }
 
-    private void CheckForWalls()
-    {
-     float mRaycastingDistance = 1f;
-    Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
+    private void TransformFlip()
+    { 
+        float Distance = 0.5f;
+        Vector3 rayDirection = (btravellingRight) ? Vector3.right : Vector3.left;
 
-        // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + rayDirection * Distance - new Vector3(0f, 0.25f, 0f), rayDirection, 0.075f);
 
-        // if we hit something, check its tag and act accordingly
         if (hit.collider != null)
         {
-            if (hit.transform.tag == "Pipe")
+            if (hit.transform.tag == "Pipe" || hit.transform.tag == "Ground")
             {
-                bIsGoingRight = !bIsGoingRight;
-                _mSpriteRenderer.flipX = bIsGoingRight;
+                btravellingRight = !btravellingRight;
+                SpriteRenderer.flipX = btravellingRight;
 
             }
         }
     }
+    private void GroundCheck()
+    {
+        float distance = 0.2f;
+        Vector3 rayDirection = Vector3.down;  // Always points straight down
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + rayDirection * distance - new Vector3(0f, 0.25f, 0f), rayDirection, 0.075f);
+
+        if (hit.collider == null || hit.transform.tag != "Ground")
+        {
+            // No ground detected, move downward
+            float verticalMovement = -FallSpeed * Time.deltaTime;
+            transform.position += new Vector3(0f, verticalMovement, 0f);
+        }
+    }
+
+
+    //Set Movement for only when Enemy is Rendered
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+     void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+
+    void OnEnable()
+    {
+        enabled = true;
+    }
+    void OnDisable()
+    {
+        enabled = false;
+    }
+
 }
