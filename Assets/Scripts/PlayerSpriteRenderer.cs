e4e4c0c0a895bb2af45d4784aed542ab118e9e6d
altuxast <55614975+altuxast@users.noreply.github.com>
2024-03-03 13:42:45 +1100

    Implemented growth sprite renderer
    Big sprite now correctly displays when magic mushroom picked-up

--- Assets/Scripts/PlayerSpriteRenderer.cs
+++ Assets/Scripts/PlayerSpriteRenderer.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class PlayerSpriteRenderer : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    private PlayerMovement playerMovement;
+    // private PlayerMovement playerMovement;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        playerMovement = GetComponent<PlayerMovement>();
+        // playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    private void Disable()
+    {
+        spriteRenderer.enabled = false;
     }
 }
